27416f8554b5e788ef880cbc415fda9257540ddc
sambaash-taylor-hickem <taylor.hickem@sambaash.com>
2022-10-22 16:37:37 +0800

    add sql

--- blockytime.py
+++ blockytime.py
@@ -23,18 +23,24 @@ import pandas as pd
 TABLES = {
     'events': []
 }
-DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+SQL_EVENT_TABLENAME = 'event'
+DATE_FORMAT = '%Y-%m-%d %H:%M'
 CSV_FILENAME = 'events.csv'
-REPORING_YEAR = 2020
-REPORING_MONTH = 12
+REPORTING_YEAR = 2020
+REPORTING_MONTH = 12
 
 
 #-----------------------------------------------------------------------------
 #main
 #-----------------------------------------------------------------------------
 def update():
+    """ this function updates the gsheet report with new blockytime events
+    :return: None
+    """
     load()
     transform_events()
+    update_db()
+
     create_report_tables()
     post_to_gsheet()
     print('report updated!')
@@ -49,12 +55,71 @@ def load():
 
 
 def update_config():
-    global REPORING_YEAR, REPORING_MONTH
+    global REPORTING_YEAR, REPORTING_MONTH
     config = db.get_sheet('hours', 'config')
     parameters = config[config['group'] == 'reporting'][
         ['parameter', 'value']].set_index('parameter')['value']
-    REPORING_YEAR = int(parameters['reporting_year'])
-    REPORING_MONTH = int(parameters['reporting_month'])
+    REPORTING_YEAR = int(parameters['reporting_year'])
+    REPORTING_MONTH = int(parameters['reporting_month'])
+
+
+#-----------------------------------------------------------------------------
+#SQLite
+#-----------------------------------------------------------------------------
+def load_sql():
+    db.load_sql()
+
+
+def update_db(new_events=None, has_duplicates=True):
+    """ this function updates the sqlite database
+    and checks for duplicates to add only the unique new events
+    """
+    global TABLES
+    if new_events is None:
+        new_events = TABLES['events']
+
+    if len(new_events) > 0:
+        if db.table_exists(SQL_EVENT_TABLENAME): #db has existing events
+            if has_duplicates:
+                #compare and remove duplicates that are already in the db
+                not_in_db = remove_db_events(new_events)
+
+                # recursive call no duplicates
+                update_db(not_in_db, has_duplicates=False)
+            else:
+                # only the incremental events
+                db.update_table(new_events, 'event', append=True)
+
+        else: #first update
+            has_duplicates = False
+            db.update_table(new_events, 'event', append=False)
+
+    if not has_duplicates:
+        db_events = db.get_table('event')
+        report_year = db_events[db_events.year == REPORTING_YEAR]
+        TABLES['events'] = report_year
+
+
+def remove_db_events(new_events):
+    # get the events from the db
+    db_events = db.get_table(SQL_EVENT_TABLENAME)
+
+    # add a datetime field to use as the unique field to both tables
+    for t in [db_events, new_events]:
+        t['start_datetime'] = t['Start'].apply(
+            lambda x: dt.datetime.strptime(x, DATE_FORMAT))
+
+    # add only the new events not already in the database
+    not_in_db = pd.concat([db_events, db_events, new_events])
+    not_in_db.drop_duplicates(
+        subset=['start_datetime'],
+        keep=False,
+        inplace=True
+    )
+    # drop the temporary datetime field
+    del not_in_db['start_datetime']
+
+    return not_in_db
 
 
 #-----------------------------------------------------------------------------
@@ -73,12 +138,14 @@ def transform_events():
     events['day'] = events['start_date'].apply(lambda x: x.day)
     events['month'] = events['start_date'].apply(lambda x: x.month)
     events['year'] = events['start_date'].apply(lambda x: x.year)
+    events['start_date'] = events['start_date'].apply(
+        lambda x: str(x))
 
     #convert duration in minutes to hours
     events['duration_hrs'] = events['Duration']/60
 
-    events = events[events['year'] == REPORING_YEAR].copy()
-    events = events[events['month'] <= REPORING_MONTH].copy()
+    events = events[events['year'] == REPORTING_YEAR].copy()
+    events = events[events['month'] <= REPORTING_MONTH].copy()
 
     #fill blank categories
     events['Event Type'].fillna('', inplace=True)
@@ -128,6 +195,19 @@ def post_to_gsheet():
     db.post_to_gsheet(subcategory_report.reset_index()[['Event Type', 'Event Object']],
                       'hours', 'subcategories',
                       input_option='USER_ENTERED')
+
+
+def timestamp_rm_seconds(datetime_str):
+    has_sec = False
+    ts_no_sec = datetime_str
+    if ':' in datetime_str:
+        dt_split = datetime_str.split(':')
+        if len(dt_split) == 3:
+            has_sec = True
+    if has_sec:
+        ts_no_sec = datetime_str[:-3]
+    return ts_no_sec
+
 
 # -----------------------------------------------------
 # Command line interface
