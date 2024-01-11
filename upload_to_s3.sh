6867f3dc9433f97066a776688325c63e72a58c4b
Taylor W Hickem <taylor.hickem@gmail.com>
2024-01-11 17:46:42 +0800

    Add files via upload

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -4,7 +4,7 @@
 
 echo upload to S3 script started
 GIT_REPO=${PWD##*/} 
-echo GIT_REPO
+echo $GIT_REPO
 layers_full_path=$GIT_REPO/$LAYERS_PATH
 echo $layers_full_path
 cfn_template_full_path=$GIT_REPO/$CFN_TEMPLATE_PATH
@@ -12,8 +12,11 @@ echo $cfn_template_full_path
 
 mkdir remote_files
 cd remote_files
+echo downloading from s3://$S3_BUCKET/git/layers_full_path
 aws s3 cp s3://$S3_BUCKET/git/layers_full_path layers.json
-aws s3 cp s3://$S3_BUCKET/git/cfn_template_full_path layers.json
+
+echo downloading from s3://$S3_BUCKET/git/cfn_template_full_path
+aws s3 cp s3://$S3_BUCKET/git/cfn_template_full_path cfn_template.yaml
 
 ls
 
