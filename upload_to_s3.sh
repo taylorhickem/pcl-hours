b5f1e681b4a9c58ce89b06b9eebddedf53556ef3
Taylor W Hickem <taylor.hickem@gmail.com>
2024-01-11 17:35:22 +0800

    Add files via upload

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -3,4 +3,19 @@
 # hello
 
 echo upload to S3 script started
-git diff --name-status $CODEBUILD_WEBHOOK_BASE_REF
+GIT_REPO=${PWD##*/} 
+echo GIT_REPO
+layers_full_path=$GIT_REPO/$LAYERS_PATH
+echo layers_full_path
+cfn_template_full_path=$GIT_REPO/$CFN_TEMPLATE_PATH
+echo cfn_template_full_path
+
+mkdir remote_files
+cd remote_files
+aws s3 cp s3://$S3_BUCKET/git/layers_full_path layers.json
+aws s3 cp s3://$S3_BUCKET/git/cfn_template_full_path layers.json
+
+ls
+
+cd .
+rm -r remote_files
