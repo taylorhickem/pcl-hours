09275b441d82bcd636f0e0847a0bf31761727f09
taylorhickem <taylor.hickem@gmail.com>
2024-01-12 15:28:39 +0800

    typos

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+files_compare () {
+  endpoint=s3://$S3_BUCKET/git/$GIT_REPO/$2
+  aws s3 cp $endpoint $remote_dir/$3
+  if [ $? == 0 ] then
+    if [ "$(cat $2)" != "$(cat $remote_dir/$3)" ] then
+      ACTION_TYPES=$ACTION_TYPES,$1
+  else
+    echo $3 not found at $endpoint
+}
+
 echo upload to S3 script started
 remote_dir=remote
 GIT_REPO=${PWD##*/}
@@ -11,22 +21,13 @@ echo CFN_TEMPLATE_PATH $CFN_TEMPLATE_PATH
 mkdir remote_files
 
 echo checking for changes in layers.json ...
-file_compare layers_update $LAYERS_PATH layers.json
+files_compare layers_update $LAYERS_PATH layers.json
 
 echo checking for changes in cfn_template.yaml ...
-file_compare stack_update $CFN_TEMPLATE_PATH cfn_template.yaml
+files_compare stack_update $CFN_TEMPLATE_PATH cfn_template.yaml
 
 rm -r remote_files
 
 ACTION_TYPES=[$ACTION_TYPES]
 echo ACTION_TYPES $ACTION_TYPES
 
-file_compare () {
-  endpoint=s3://$S3_BUCKET/git/$GIT_REPO/$2
-  aws s3 cp $endpoint $remote_dir/$3
-  if [ $? == 0 ]
-    if [ "$(cat $2)" != "$(cat $remote_dir/$3)" ] then
-      ACTION_TYPES=$ACTION_TYPES,$1
-  else
-    echo $3 not found at $endpoint
-}
